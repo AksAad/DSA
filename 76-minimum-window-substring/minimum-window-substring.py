5f023889b495976a49190516bb451b3fe8de2732
Akshat Soni <soni.akshat925@gmail.com>
2025-10-11 11:28:53 +0530

    Time: 67 ms (69.98%) | Memory: 18.1 MB (92.89%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -4,24 +4,26 @@ class Solution:
             return ""
         have = 0
         t_count = Counter(t)
-        window_count = {}
-        left = 0
         need = len(t_count)
-        min_length = None
+        window_count = {}
         min_start = None
-        for right,char in enumerate(s):
+        min_length = None
+        left = 0
+        for right, char in enumerate(s):
             window_count[char] = window_count.get(char,0) + 1
             if char in t_count and window_count[char] == t_count[char]:
                 have += 1
             while have == need:
                 current_length = right - left + 1
-                if min_length is None or current_length < min_length:
-                    min_length = current_length
+                if min_start == None or current_length < min_length:
                     min_start = left
+                    min_length = current_length
                 window_count[s[left]] -= 1
                 if s[left] in t_count and window_count[s[left]] < t_count[s[left]]:
                     have -= 1
                 left += 1
-        if min_start is None:
-            return "" 
-        return s[min_start:min_start + min_length]
+        if min_start == None:
+            return ""
+        return s[min_start:min_length + min_start]
+
+
