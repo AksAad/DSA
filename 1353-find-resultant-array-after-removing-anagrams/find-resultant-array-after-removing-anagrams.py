2badf99835dac5f23bd92a63e85ee061f96bec91
Akshat Soni <soni.akshat925@gmail.com>
2025-10-13 11:12:44 +0530

    Time: 34 ms (5.52%) | Memory: 17.7 MB (94.98%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -1,13 +1,10 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        #wtf are anagrams bruh
-        # use Counter to store count of two consecutive words
-        ans = []
         i = 0
         while i in range(0,len(words)-1):
             a1 = Counter(words[i])
             b1 = Counter(words[i+1])
-            if a1 == b1: #this is an anagram
+            if a1 == b1:
                 words.remove(words[i+1])
             else:
                 i+=1
