9d90776364ff7540616c1786b6a52e9bc81fd932
Akshat Soni <soni.akshat925@gmail.com>
2026-01-31 11:59:49 +0530

    Time: 12 ms (53.20%) | Memory: 19.4 MB (34.07%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return(sorted(s) == sorted(t))
+        freq1 = defaultdict(int)
+        freq2 = defaultdict(int)
+        for c in s:
+            freq1[c] += 1
+        for c in t:
+            freq2[c] += 1
+        return freq1 == freq2
